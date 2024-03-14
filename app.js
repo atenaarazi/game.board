db46a6e418f01b414976333a74a16e08cd40026b
atenaarazi <atenarazii@gmail.com>
2024-03-14 12:53:11 +0330

    littleThings

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ const squares = Array.from(document.querySelectorAll(".grid div"));
 console.log(squares);
 
 const alienInvaders = [
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 30, 31,
-  32, 33, 34, 35, 36, 37, 38, 39,
+   0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+   15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 
+   30, 31,32, 33, 34, 35, 36, 37, 38, 39
 ];
 
 function draw() {
@@ -62,29 +63,23 @@ function moveShooter(e) {
         isGoingRight = false;
       }
     }
- if(leftEdge && !isGoingRight)
-{
-    for (let i = 0; i < alienInvaders.length; i++){
-        alienInvaders[i] += width - 1
-        direction = 1
-        isGoingRight = true
-
-
+    if (leftEdge && !isGoingRight) {
+      for (let i = 0; i < alienInvaders.length; i++) {
+        alienInvaders[i] += width - 1;
+        direction = 1;
+        isGoingRight = true;
+      }
     }
-}
 
-for(let i = 0; i < alienInvaders.length; i++){
-    alienInvaders[i] += direction
+    for (let i = 0; i < alienInvaders.length; i++) {
+      alienInvaders[i] += direction;
+    }
 
-}
-
-
-
-    draw()
-    if( squares [currentShooterIndex].classList.contains('invader')){
-        resultsDisplay.innerHTML = 'GAME OVER'
-        clearInterval
+    draw();
+    if (squares[currentShooterIndex].classList.contains("invader")) {
+      resultsDisplay.innerHTML = "GAME OVER";
+      clearInterval;
     }
   }
-  invadersId = setInterval(moveInvaders, 600)
+  invadersId = setInterval(moveInvaders, 600);
 }
